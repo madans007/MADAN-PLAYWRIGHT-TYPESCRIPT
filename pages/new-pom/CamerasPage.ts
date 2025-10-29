76f45d210dee4473cc7fb5997690d91e53c6db15
madans007 <madans007007@gmail.com>
2025-10-29 17:24:43 +0530

    file updates

--- pages/new-pom/CamerasPage.ts
+++ pages/new-pom/CamerasPage.ts
@@ -1,32 +1,32 @@
-import {Page, Locator} from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 export class Cameras {
 
-     private readonly page: Page;
-     private readonly camerasPage: Locator;
-     private readonly cameraProductNames: Locator;
+   private readonly page: Page;
+   private readonly camerasPage: Locator;
+   private readonly cameraProductNames: Locator;
 
-     constructor (page: Page){
-        this.page = page;
-        this.camerasPage  = page.locator('div.col-sm-9 h2');
-        this.cameraProductNames =  page.locator('div.caption h4 a');
-     }
+   constructor(page: Page) {
+      this.page = page;
+      this.camerasPage = page.locator('div.col-sm-9 h2');
+      this.cameraProductNames = page.locator('div.caption h4 a');
+   }
 
-     async getCamerasPageTitle(){
-        const camerasPageText: string = await this.camerasPage.innerText();
-        return camerasPageText;
-     }
+   async getCamerasPageTitle() {
+      const camerasPageText: string = await this.camerasPage.innerText();
+      return camerasPageText;
+   }
 
-     async clickAndSelectProduct(productName: string){
-        const allCameraProducts: number= await this.cameraProductNames.count();
+   async clickAndSelectProduct(productName: string) {
+      const allCameraProducts: number = await this.cameraProductNames.count();
 
-        for (let i = 0; i < allCameraProducts; i++) {
-            const cameraName: string = await this.cameraProductNames.nth(i).innerText();
-            console.log(`Name of Item:, ${cameraName}`);
-            if (cameraName === productName) {
-                await this.cameraProductNames.nth(i).click();
-                return;
-            }
-        }
-     }
+      for (let i = 0; i < allCameraProducts; i++) {
+         const cameraName: string = await this.cameraProductNames.nth(i).innerText();
+         console.log(`Name of Item:, ${cameraName}`);
+         if (cameraName === productName) {
+            await this.cameraProductNames.nth(i).click();
+            return;
+         }
+      }
+   }
 }
