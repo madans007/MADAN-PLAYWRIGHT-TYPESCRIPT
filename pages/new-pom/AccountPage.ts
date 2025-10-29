8e11bdb34180f2edfa75b4d2029769a9d3d8b88e
madans007 <madans007007@gmail.com>
2025-10-29 11:40:33 +0530

    account page

---
+++ pages/new-pom/AccountPage.ts
@@ -0,0 +1,24 @@
+import { Page, Locator } from '@playwright/test';
+
+
+export class Accountpage {
+
+    private readonly page: Page;
+    private readonly navBarItems: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.navBarItems = page.locator('ul.navbar-nav li a');
+    }
+
+    async clickOnCameras(itemName: string) {
+        const allNavBarItems: number = await this.navBarItems.count();
+        for (let i = 0; i < allNavBarItems; i++) {
+            const navBarItemName: string = await this.navBarItems.nth(i).innerText();
+            if (navBarItemName === itemName) {
+                await this.navBarItems.nth(i).click();                       // selects particluar item of index i.
+                return;                     // break - is not needed --> Use when you want to stop looping but do more work after.
+            }
+        }
+    }
+}
