53774971bc367acd05e08e1c70de00104cc77d0a
madans007 <madans007007@gmail.com>
2025-11-14 12:14:11 +0530

    file update

--- utils/dataProvider.ts
+++ utils/dataProvider.ts
@@ -2,13 +2,13 @@
 
 import fs from 'fs';
 import { parse } from 'csv-parse/sync';
-//import * as xlsx from xlsx;   //only if you have added xlsx dependency and working with excel
+//import * as xlsx from xlsx;                        //only if you have added xlsx dependency and working with excel
 
 export class DataProvider {
 
     static getTestDataFromJson(filePath: string) {
         const raw = fs.readFileSync(filePath, 'utf8');      // read file synchronously
-        return JSON.parse(raw); 
+        return JSON.parse(raw);                             // converts JSON formatted string to a js object.
     }
 
     static getTestDataFromCsv(filePath: string) {
@@ -18,6 +18,16 @@ export class DataProvider {
         });
     }
 
+    /*
+    static getTestDataFromExcel(filePath: string, sheetName?: string) {         // to read Excel file
+        const workbook = xlsx.readFile(filePath);                       // read the Excel file
+        const sheet = sheetName
+            ? workbook.Sheets[sheetName]
+            : workbook.Sheets[workbook.SheetNames[0]];              // default to first sheet if not provided
+
+        return xlsx.utils.sheet_to_json(sheet, { defval: null });   // convert sheet to JSON (array of objects)
+    }
+    */
 
 }
 
